f82a647bccf62cf9f118998cdb85518bad9d9782
demonhandz <demonhandz@gmail.com>
2023-05-15 23:33:24 +0300

    update

--- src/BasicSyntaxConditionalStatementsandLoops/VendingMachine07.java
+++ src/BasicSyntaxConditionalStatementsandLoops/VendingMachine07.java
@@ -6,13 +6,62 @@ public class VendingMachine07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
+        double totalMoney = 0.0;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(i + " ");
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("Start")) {
+                break;
             }
-            System.out.println();
+
+            double coin = Double.parseDouble(input);
+
+            if (coin == 0.1 || coin == 0.2 || coin == 0.5 || coin == 1 || coin == 2) {
+                totalMoney += coin;
+            } else {
+                System.out.printf("Cannot accept %.2f%n", coin);
+            }
         }
+
+        while (true) {
+            String product = scanner.nextLine();
+
+            if (product.equals("End")) {
+                break;
+            }
+
+            double price = 0.0;
+
+            switch (product) {
+                case "Nuts":
+                    price = 2.0;
+                    break;
+                case "Water":
+                    price = 0.7;
+                    break;
+                case "Crisps":
+                    price = 1.5;
+                    break;
+                case "Soda":
+                    price = 0.8;
+                    break;
+                case "Coke":
+                    price = 1.0;
+                    break;
+                default:
+                    System.out.println("Invalid product");
+                    continue;
+            }
+
+            if (totalMoney >= price) {
+                totalMoney -= price;
+                System.out.printf("Purchased %s%n", product);
+            } else {
+                System.out.println("Sorry, not enough money");
+            }
+        }
+
+        System.out.printf("Change: %.2f%n", totalMoney);
     }
 }
