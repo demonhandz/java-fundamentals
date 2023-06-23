ca9f43e3c96fb9363e520e559cf430620359edc5
Georgi Danailov <70630746+demonhandz@users.noreply.github.com>
2023-06-23 23:57:07 +0300

    update

---
+++ src/Methods/nXnMatrix_07.java
@@ -0,0 +1,21 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class nXnMatrix_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine()); //number of rows = number of columns
+        printMatrix(n);
+
+    }
+    public static void printMatrix(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+    }
+}
