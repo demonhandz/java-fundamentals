eef117dc8f964d21da90ce4a0cc72811826b8a17
Georgi Danailov <70630746+demonhandz@users.noreply.github.com>
2023-07-16 22:12:43 +0300

    update

---
+++ src/OrderByAge_06/Main.java
@@ -0,0 +1,32 @@
+package OrderByAge_06;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Person> peopleList = new ArrayList<>();
+        String data = scanner.nextLine();
+
+        while (!data.equals("End")) {
+            String name = data.split("\\s+")[0];
+            String id = data.split("\\s+")[1];
+            int age = Integer.parseInt(data.split("\\s+")[2]);
+
+            Person person = new Person(name, id, age);
+            peopleList.add(person);
+
+            data = scanner.nextLine();
+        }
+
+        peopleList.sort(Comparator.comparing(Person::getAge));
+
+        for (Person person : peopleList) {
+            System.out.println(person.getName() + " with ID: " + person.getId() + " is " + person.getAge() + " years old.");
+        }
+    }
+}
