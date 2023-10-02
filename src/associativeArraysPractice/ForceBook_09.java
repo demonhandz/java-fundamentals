ee5a70c79581c4427d22cf07633da38f6534c2ad
Georgi Danailov <70630746+demonhandz@users.noreply.github.com>
2023-10-02 22:42:01 +0300

    update

--- src/associativeArraysPractice/ForceBook_09.java
+++ src/associativeArraysPractice/ForceBook_09.java
@@ -22,17 +22,51 @@ public class ForceBook_09 {
             if (!teams.containsKey(team)) {
                 teams.put(team, new ArrayList<>());
             } else {
+                //2. we have such a team
+                //we add the player to the team, if he doesn't exist in another team
                 boolean isExist = false;
+                //isExist = true -> the player exists in another team
+                //isExist = false -> the player doesn't exist in another team
+                for (List<String> list : teams.values()) {
+                    if (list.contains(player)) {
+                        isExist = true;
+                        break;
+                    }
+                }
 
+                if (!isExist) {
+                    teams.get(team).add(player);
+                }
             }
 
         } else if (input.contains(" -> ")) {
+                String player = input.split(" -> ")[0];
+                String team = input.split(" -> ")[1];
 
+
+                //1.remove player if hje exist in a team
+                teams.entrySet().forEach(entryTeam -> entryTeam.getValue().remove(player));
+
+                //2.move to new team.
+                //2.1 does such team exist
+            if (!teams.containsKey(team)) {
+                teams.put(team, new ArrayList<>());
+                teams.get(teams).add(player);
+            }
+            //2.2 if you try to move the player in an already existing team
+            else {
+                teams.get(team).add(player);
+            }
+            System.out.printf("%s joins the %s side!", player, team);
         }
-
-
-
             input = scanner.nextLine();
         }
+        teams.entrySet().stream().filter(teamEntry -> teamEntry.getValue().size() > 0) //оставяме само тези записи, които отговарят на условието
+                .forEach(entry -> {
+                    //entry: key (team) -> value (list of players)
+                    //"Side: {forceSide}, Members: {forceUsers.Count}
+                    System.out.printf("Side: %s, Members: %d%n", entry.getKey(), entry.getValue().size());
+                    entry.getValue().forEach(player -> System.out.println("! " + player));
+                });
     }
 }
---
+++ src/training/Vacation.java
@@ -0,0 +1,69 @@
+package training;
+
+import java.util.Scanner;
+
+public class Vacation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int groupSize = Integer.parseInt(scanner.nextLine());
+        String groupType = scanner.nextLine();
+        String dayOfWeek = scanner.nextLine();
+
+        double pricePerPerson = 0;
+
+        switch (groupType) {
+            case "Students":
+                switch (dayOfWeek) {
+                    case "Friday":
+                        pricePerPerson = 8.45;
+                        break;
+                    case "Saturday":
+                        pricePerPerson = 9.80;
+                        break;
+                    case "Sunday":
+                        pricePerPerson = 10.46;
+                        break;
+                }
+                break;
+            case "Business":
+                switch (dayOfWeek) {
+                    case "Friday":
+                        pricePerPerson = 10.90;
+                        break;
+                    case "Saturday":
+                        pricePerPerson = 15.60;
+                        break;
+                    case "Sunday":
+                        pricePerPerson = 16;
+                        break;
+                }
+                break;
+                    case "Regular":
+                        switch (dayOfWeek) {
+                            case "Friday":
+                                pricePerPerson = 15;
+                                break;
+                            case "Saturday":
+                                pricePerPerson = 20;
+                                break;
+                            case "Sunday":
+                                pricePerPerson = 22.50;
+                                break;
+                        }
+                        break;
+        }
+
+        double totalPrice = pricePerPerson * groupSize;
+
+        if (groupType.equals("Students") && groupSize >= 30) {
+            totalPrice -= totalPrice * 0.15;
+        } else if (groupType.equals("Business") && groupSize >= 100) {
+            totalPrice -= (10 * pricePerPerson);
+        } else if (groupType.equals("Regular") && groupSize >= 10 && groupSize <= 20) {
+            totalPrice -= totalPrice * 0.05;
+        }
+
+        System.out.printf("Total price: %.2f", totalPrice);
+    }
+}
